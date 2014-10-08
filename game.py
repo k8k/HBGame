50f1539e9571ae3e49545d8a27b8ec243d1adb21
Hackbright Student <info@hackbrightacademy.com>
2014-10-08 16:39:16 -0700

    New version. Up to keyboard movement.

--- game.py
+++ game.py
@@ -9,13 +9,78 @@ GAME_BOARD = None
 DEBUG = False
 ######################
 
-GAME_WIDTH = 3
-GAME_HEIGHT = 3
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
 
 #### Put class definitions here ####
-pass
+class Rock(GameElement):
+    IMAGE = "Rock"
+
+class Character(GameElement):
+    IMAGE = "Princess"
+
+    def next_pos(self, direction):
+        if direction == "up":
+            return(self.x, self.y-1)
+        elif direction == "down":
+            return(self.x, self.y+1)
+        elif direction == "left":
+            return(self.x-1, self.y)
+        elif direction == "right":
+            return(self.x+1, self.y)
+        return None
+
+    def keyboard_handler(self, symbol, modifier):
+        direction = None
+        if symbol == key.UP:
+            direction = "up"
+        elif symbol == key.DOWN:
+            direction = "down"
+        elif symbol == key.LEFT:
+            direction = "left"
+        elif symbol == key.RIGHT:
+            direction = "right"
+
+        self.board.draw_msg("[%s] moves %s" % (self.IMAGE, direction))
+
+        if direction:
+            next_location = self.next_pos(direction)
+
+            if next_location:
+                next_x = next_location[0]
+                next_y = next_location[1]
+                self.board.del_el(self.x, self.y)
+                self.board.set_el(next_x, next_y, self)
+    
+
+
+
+
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
-    pass
+    GAME_BOARD.draw_msg("This game is awesome.")
+
+    rock_positions = [
+        (2,1),
+        (1,2),
+        (3,2),
+        (2,3),
+    ]
+
+    rocks = []
+
+    for pos in rock_positions:
+        rock = Rock()
+        GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(pos[0], pos[1], rock)
+        rocks.append(rock)
+
+    for rock in rocks:
+        print rock
+
+    player = Character()
+    GAME_BOARD.register(player)
+    GAME_BOARD.set_el(2,2,player)
+    print player
