e0369d18f897584c2030fe6ff0b17ee7fa5b2498
Hackbright Student <info@hackbrightacademy.com>
2014-10-08 18:04:21 -0700

    The princess gets the gems, the cat cuts down the trees. Working on restricting bounds

--- game.py
+++ game.py
@@ -9,8 +9,8 @@ GAME_BOARD = None
 DEBUG = False
 ######################
 
-GAME_WIDTH = 5
-GAME_HEIGHT = 5
+GAME_WIDTH = 7
+GAME_HEIGHT = 7
 
 #### Put class definitions here ####
 class Gem(GameElement):
@@ -19,15 +19,38 @@ class Gem(GameElement):
     def interact(self, player):
         player.inventory.append(self)
         GAME_BOARD.draw_msg("You just acquired a gem! You have %d items!" % len(player.inventory))
-
-
+        if len(player.inventory) > 4:
+            player.change_image("Cat")
+            GAME_BOARD.draw_msg("You are now a CAT! Meow")
 
 class Rock(GameElement):
     IMAGE = "Rock"
     SOLID = True
 
+class Door(GameElement):
+    IMAGE = "DoorClosed"
+    SOLID = True
+    def interact(self, player):
+        self.change_image("DoorOpen")
+
+
+class Tree(GameElement):
+    IMAGE = "TallTree"
+    # SOLID = True 
+
+    def interact(self, player):
+        if player.IMAGE != "Cat":
+            self.SOLID = True
+        else:
+            self.SOLID = False
+    # SOLID = False
+
 class Character(GameElement):
     IMAGE = "Princess"
+
+    # def transform(self,inventory):
+    #     if len(self.inventory) > 2:
+    #         self.change_image("Cat")
 
     def next_pos(self, direction):
         if direction == "up":
@@ -57,13 +80,18 @@ class Character(GameElement):
             next_location = self.next_pos(direction)
 
             if next_location:
+        
+            # else:
                 next_x = next_location[0]
                 next_y = next_location[1]
+
+                ###if next_x > 6 or next_x < 0:  ### BEGINNING OF BOUNDS RESTRICTION
 
                 existing_el = self.board.get_el(next_x, next_y)
 
                 if existing_el:
                     existing_el.interact(self)
+
 
                 if existing_el and existing_el.SOLID:
                     self.board.draw_msg("There's something in my way!")
@@ -101,24 +129,45 @@ def initialize():
 
     rocks[-1].SOLID = False
 
-    for rock in rocks:
-        print rock
 
     player = Character()
     GAME_BOARD.register(player)
     GAME_BOARD.set_el(2,2,player)
-    print player
 
-    GAME_BOARD.draw_msg("This game is awesome.")
+    GAME_BOARD.draw_msg("Can you chop down the trees? Welcome to our deforestation game.")
 
-    gem = Gem()
-    GAME_BOARD.register(gem)
-    GAME_BOARD.set_el(3,1, gem)
+    
+    gem_positions = [
+        (3,1),
+        (1,3),
+        (4,4),
+        (3,3),
+        (0,4),
+        ]
 
-    gem2 = Gem()
-    gem2.IMAGE = "OrangeGem"
-    GAME_BOARD.register(gem2)
-    GAME_BOARD.set_el(1,3, gem)
+    gems = []
 
+    for pos in gem_positions:
+        gem = Gem()
+        GAME_BOARD.register(gem)
+        GAME_BOARD.set_el(pos[0], pos[1], gem)
+        gems.append(gem)
+
+
+    tree_positions = [
+        (4,1),
+        (2,6),
+        (6,6),
+        (5,6),
+        (0,0),
+        ]
+
+    trees = []
+
+    for pos in tree_positions:
+        tree = Tree()
+        GAME_BOARD.register(tree)
+        GAME_BOARD.set_el(pos[0], pos[1], tree)
+        trees.append(tree)
 
 
